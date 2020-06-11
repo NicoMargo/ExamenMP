275ad22fe75241040809345859b396dde0df517b
unknown <nicolasmargossian@gmail.com>
2020-06-11 01:53:39 -0300

    test

--- ExamenMP/ExamenMP/Controllers/BackofficeController.cs
+++ ExamenMP/ExamenMP/Controllers/BackofficeController.cs
@@ -6,18 +6,17 @@ using System.Web.Mvc;
 using MercadoPago.DataStructures.Preference;
 using MercadoPago.Resources;
 using MercadoPago.Common;
-using System.Web.UI.WebControls;
 
 namespace ExamenMP.Controllers
 {
-    public class HomeController : Controller      
+    public class BackofficeController : Controller
     {
-        public ActionResult ian()
+        
+        public ActionResult Checkout(string image, string prodName, decimal price)
         {
-            return View();
-        }
-        public ActionResult Index()
-        {
+            ViewBag.image = image;
+            ViewBag.prodName = prodName;
+            ViewBag.price = price;
             try
             {
                 if (MercadoPago.SDK.AccessToken == null)
@@ -29,59 +28,41 @@ namespace ExamenMP.Controllers
             {
                 MercadoPago.SDK.AccessToken = "APP_USR-6317427424180639-042414-47e969706991d3a442922b0702a0da44-469485398";
             }
-          
 
-            /*Payment pago = new Payment()
-            {
-                TransactionAmount = float.Parse("100"),
-                Description = "Título del producto",
-                PaymentMethodId = "rapipago"
-            };*/
 
             Preference preference = new Preference();
             preference.Items.Add(
               new Item()
               {
-                  Title = "Camion",
+                  
+                  Title = prodName,
                   Description = "Dispositivo móvil de Tienda e-commerce",
                   Id = "1234",
                   Quantity = 1,
                   CurrencyId = CurrencyId.ARS,
-                  UnitPrice = 20,
-                  PictureUrl = "url",
+                  UnitPrice = price,
+                  PictureUrl = image,
               }
             );
 
-            
 
            
-
-            Payer payer = new Payer()
-            {
-                Name = "Lalo",
-                Surname = "Landa",
-                Email = "test_user_63274575@testuser.com",
-                Phone = new Phone()
-                {
-                    Number = "22223333",
-                    AreaCode = "11"
-                },
-                Address = new Address()
-                {
-                    StreetName ="False",
-                    StreetNumber = 123,
-                    ZipCode = "1111"
-
-                }
-            };            
-
-            preference.Payer = payer;
+            preference.ExternalReference = "nicomargo@protonmail.com";
             preference.Save();
             ViewBag.id = preference.Id;
             ViewBag.Title = "Home Page";
 
             return View();
-        }        
+            
+        }
 
+        public bool Webhook(JsonResult json = null)
+        {
+            Response.StatusCode = 200;
+            Response.End();
+            return true;
+        }
+
+       
     }
 }
